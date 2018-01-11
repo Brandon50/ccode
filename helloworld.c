0e993b07c0a2f1a40df465ea61930837539ae6b2
Brandon50 <Brandonweston77@yahoo.com>
2018-01-11 09:36:53 -0800

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+// ******* helloword.c *******
+//save file as helloworld.c		
+// compile $gcc helooworld.c -o helloworld.o
+//run ./helloworld.o
+
+#inclued <stdio.h>
+
+int main(){
+    
+    printf ("hello world \n\n");
+    
+}
+
+
